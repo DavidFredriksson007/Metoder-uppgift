9299ae95045e98f69365405828f7c330a4f8c031
Elev <Elev@TEB123013>
2019-10-21 11:57:33 +0200

    Metoder uppgift 1

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+         public static void Main()
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+        }
+
+        static double CirkelOmkrets(double radie)
+        {
+
+            return (radie + radie) * (3.14);
+        }
+
+        static double CirkelArea(double radie)
+        {
+
+            return (radie * radie * 3.14);
+ 
+        }
+        
+    } 
+
+}
+
