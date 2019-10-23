e02c26b0cb4c0f1fcff2e1356ce84027b51cba86
Elev <Elev@TEB123013>
2019-10-23 14:52:05 +0200

    metoder3

---
+++ metoder3/metoder3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metoder3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int a = 10, b = 20;
+            BytPlats(ref a, ref b);
+            Console.WriteLine(a +" "+ b);
+           
+            
+        }
+
+        public static void BytPlats(ref int a, ref int b)
+        {
+            a = 20;
+            b = 10;
+      
+
+            
+        }
+
+
+
+    }
+}
